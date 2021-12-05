bceaa03c7615895ba0deb2011320508f15856447
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2021-12-05 11:15:01 +0100

    Fetches the challenges list

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-async function Heroku(){
-    let url = `https://web2-fullstack-teamwork.herokuapp.com/`;
-    let resp = await fetch(url);
-    return await resp.json()
+'Use strict';
+
+let challengesDataList;
+let challengesListHTML = "";
+
+fetchChallenges();
+
+function fetchChallenges (){
+    console.log("Fetch!")
+
+    fetch(`https://web2-fullstack-teamwork.herokuapp.com/challenges`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            console.log("dataList:", data)
+            challengesDataList = data
+        })
 }
 
-window.onload = function(){
-    console.log('Loaded!');
+window.onload = function () {
 
-    async function run(){
-        let data = await Heroku()
-        console.log(data)
+    setTimeout(displayChallenges, 3000)
+
+    function displayChallenges() {
+        console.log('Loaded!');
+
+        challengesDataList.forEach(e => {
+            challengesListHTML += `<article id="${e._id}" class="listArticle">
+            <h3>${e.name}</h3>
+            <button>Delete</button>
+            <button>Edit</button>
+        </article>`
+        })
+
+        console.log(challengesListHTML)
+        document.getElementById("challengesList").innerHTML = challengesListHTML;
     }
-
-    run();
 }
